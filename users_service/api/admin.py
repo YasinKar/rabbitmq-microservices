7a9e81272fd3a193cc7feefa4c67a4edb882c430
Yasin Karbasi <yasinkardev@gmail.com>
2025-02-07 13:17:24 +0330

    added models and serializers

--- users_service/api/admin.py
+++ users_service/api/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'username', 'email', 'is_active')
+    list_filter = ('is_active', )
+    list_editable = ('is_active',)
+    list_display_links = ('first_name', 'last_name', 'username', 'email')
+    search_fields = ('first_name', 'last_name', 'username', 'email')
